77c4d6704a121b2ede478b369c4e75356b6459c1
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-26 11:42:12 -0700

    Contoller Setup with MiddleWare

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("User has been created.");
   } catch (err) {
-    return res.status(500).send("not working!");
+    next(err);
   }
 };
 
