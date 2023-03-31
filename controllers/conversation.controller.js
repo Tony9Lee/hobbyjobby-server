f6ed0f3a78de646ed8157d979b54a7deecb73b35
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-31 14:15:27 -0700

    Modified Conversation Messages & Controller

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -40,6 +40,7 @@ export const updateConversation = async (req, res, next) => {
 export const getSingleConversation = async (req, res, next) => {
   try {
     const conversation = await Conversation.findOne({ id: req.params.id });
+    if (!conversation) return next(createError(404, "Not Found"));
     res.status(200).send(conversation);
   } catch (err) {
     next(err);
